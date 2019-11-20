854e71b1d63ca4497c6dc1e96d2935e1b0e7c7f8
Morekolodi Motsumi <motsumilab@gmail.com>
2019-11-20 13:54:41 +0200

    Initial commit

---
+++ app/src/main/java/com/mcs/homescreedesign/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mcs.homescreedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnlogin = findViewById(R.id.openLogin);
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
